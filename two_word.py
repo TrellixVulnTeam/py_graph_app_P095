c00970c5ef0dc813fe52a837656c98be502be67b
stevekutz <stkutz@gmail.com>
2020-08-20 17:45:05 -0500

    added two_word prob description

---
+++ two_word.py
@@ -0,0 +1,25 @@
+# given two words(begin_word and end_word) and a dictionary's word list, 
+# return the shortest transformation sequence from begin_word to end_word
+# such that:
+#
+# Only 1 letter changed at a time
+# each transformed word must exist in the word list. note the begin_word
+# is not a transformed word
+#
+# Note:
+#  Return NONE if there no such transformation sequence
+# All words contain only lowercase alphanumeric characters
+# You may assum no duplicates in the word list]
+# You may asssume begin_word and end_word are non-empty and sre not 
+# the same
+# 
+# Sample:
+# begin_word = "hit"
+# end_word = "cog"
+# return ['hit', 'hot', 'cot', 'cog']
+#
+# begin_word = "hungry"
+# end_word = "happy"
+# None
+
+
