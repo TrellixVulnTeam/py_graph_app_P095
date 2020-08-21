f34b87af04fd96621d7a928e795b703d112fe212
stevekutz <stkutz@gmail.com>
2020-08-21 11:14:21 -0500

    added another word_ladder

--- total_edges.py
+++ total_edges.py
@@ -8,16 +8,17 @@ def total_edges(n, directed = False):
 
 
 for i in range(10):
-    print(f' num edges {i} \t directed max total edges: {total_edges(i)} \t unidrected total edges {total_edges(i, True)}')    
+    print(f' num edges {i} \t directed max total edges: {total_edges(i)} \t undirected max total edges {total_edges(i, True)}')    
 
-#  num edges 0 total edges: 0
-#  num edges 1 total edges: 0
-#  num edges 2 total edges: 1
-#  num edges 3 total edges: 3
-#  num edges 4 total edges: 6
-#  num edges 5 total edges: 10
-#  num edges 6 total edges: 15
-#  num edges 7 total edges: 21
-#  num edges 8 total edges: 28  
+#  num edges 0     directed max total edges: 0     undirected total edges 0
+#  num edges 1     directed max total edges: 0     undirected total edges 0
+#  num edges 2     directed max total edges: 1     undirected total edges 2
+#  num edges 3     directed max total edges: 3     undirected total edges 6
+#  num edges 4     directed max total edges: 6     undirected total edges 12
+#  num edges 5     directed max total edges: 10    undirected total edges 20
+#  num edges 6     directed max total edges: 15    undirected total edges 30
+#  num edges 7     directed max total edges: 21    undirected total edges 42
+#  num edges 8     directed max total edges: 28    undirected total edges 56
+#  num edges 9     directed max total edges: 36    undirected total edges 72 
 # 
 #   
